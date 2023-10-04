6a25086e98591d1f0c04bc35845996f37632c096
guillezdev <guillezdev@gmail.com>
2023-10-03 23:48:14 -0400

    some changes

--- dist/vendors/js/acor-modal.js
+++ dist/vendors/js/acor-modal.js
@@ -6,14 +6,7 @@ const acorModalItem = document.querySelectorAll('.acor-modal-item');
 btnAcorModal.forEach((element , index) => {
     if (window.innerWidth < 976) {
         btnAcorModal[index].addEventListener('click', () => { 
-            if(acorModalItem[index].classList.contains('expand-acordion')){
-                acorModalItem[index].classList.remove('expand-acordion');
-                acorModalItem[index].classList.add('contract-acordion');
-            }
-            else{
-                acorModalItem[index].classList.remove('contract-acordion');
-                acorModalItem[index].classList.add('expand-acordion');
-            }
+                acorModalItem[index].classList.toggle('expand-acordion');
          })
     }
 });
