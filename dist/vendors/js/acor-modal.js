09e18f95bf3b326257a7a0fb2e8281d7fcd0f110
guillezdev <guillezdev@gmail.com>
2023-10-04 18:26:56 -0400

    carousel - acordion and modal

--- dist/vendors/js/acor-modal.js
+++ dist/vendors/js/acor-modal.js
@@ -1,27 +1,85 @@
 
 const btnAcorModal = document.querySelectorAll('.acor-modal');
 const acorModalItem = document.querySelectorAll('.acor-modal-item');
+const modalLeft = document.querySelector('#modal-left');
+const modalRight = document.querySelector('#modal-right');
+const closeModal = document.querySelectorAll('.close-modal')
+
+//Por defecto estan con display none.
+modalLeft.classList.add('hidden')
+modalRight.classList.add('hidden')
+
+btnAcorModal.forEach((element, index) => {
+
+    btnAcorModal[index].addEventListener('click', () => {
+
+        if (window.innerWidth < 976) {
+            acorModalItem.forEach((e) => { e.classList.remove('expand-acordion') })//Esta linea es opcional, es para q solo se vea un acordeon a la vez, ademas siempre se vera almenos uno.
+            acorModalItem[index].classList.toggle('expand-acordion');
+        }
+        if (window.innerWidth > 976) {
+            if (index >= 7) {
+                modalRight.classList.toggle('hidden')
+                modalLeft.classList.add('hidden')
+                
+                console.log(index);
+            }
+            else if (index < 7) {
+                modalLeft.classList.toggle('hidden')
+                modalRight.classList.add('hidden')
+                console.log(index);
+            }
+        }
+    })
+});
+
+//Para la x q cierra los modales.
+closeModal.forEach((el, i) => {
+    closeModal[i].addEventListener('click', () => {
+        modalLeft.classList.add('hidden')
+        modalRight.classList.add('hidden')
+    })
+})
 
 
-btnAcorModal.forEach((element , index) => {
-    if (window.innerWidth < 976) {
-        btnAcorModal[index].addEventListener('click', () => { 
-                acorModalItem[index].classList.toggle('expand-acordion');
-         })
-    }
+const carousels = document.querySelectorAll(".carousel-container");
+const controlsList = document.querySelectorAll(".guias");
+
+carousels.forEach((carousel, carouselIndex) => {
+  const controls = controlsList[carouselIndex];
+  const controlItems = controls.querySelectorAll("div[data-index]");
+
+  setActiveControl(0);
+
+  function setActiveControl(index) {
+    controlItems.forEach((item, i) => {
+      if (i === index) {
+        item.classList.add("bg-guias");
+      } else {
+        item.classList.remove("bg-guias");
+      }
+    });
+  }
+
+  controlItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      const cardWidth = carousel.querySelector(".snap-start").offsetWidth;
+      carousel.scroll({ left: cardWidth * index, behavior: "smooth" });
+    });
+  });
+
+ 
+  carousel.addEventListener("scroll", () => {
+    const cardWidth = carousel.querySelector(".snap-start").offsetWidth;
+    const scrollPos = carousel.scrollLeft;
+    const activeControlIndex = Math.floor(scrollPos / cardWidth);
+    setActiveControl(activeControlIndex);
+  });
 });
 
 
-btnAcorModal.forEach((element , index) => {
-    if (window.innerWidth > 976) {
-        btnAcorModal[index].addEventListener('click', () => { 
-              if (index > 1) {
-                console.log(index);
-              }
-              else{
-                console.log(index);
-              }
-         })
-    }
-});
+
+
+
+
 
